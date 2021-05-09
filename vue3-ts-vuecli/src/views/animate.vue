f877945ed0e86077be62a1dfb4289856b1134013
Yukilwc <misaka1033@gmail.com>
2021-05-09 23:47:36 +0800

    u

--- vue3-ts-vuecli/src/views/animate.vue
+++ vue3-ts-vuecli/src/views/animate.vue
@@ -2,17 +2,27 @@
   <div class="container">
     <div class="h1">动画测试</div>
     <div class="h2">吸顶sticky实验</div>
-    <div class="content sticky-panel">
+    <!-- <div class="content sticky-panel">
       <div class="left"></div>
       <div class="right"></div>
-    </div>
+    </div> -->
     <div class="h2">tab切换的动画</div>
+    <el-button @click="switchShow">switch</el-button>
+    <div class="content transition">
+      <transition name="name" mode="out-in">
+        <div class="list" :key="key">
+          <div class="item" v-for="(item, index) in test.list" :key="index">
+            {{ item }}
+          </div>
+        </div>
+      </transition>
+    </div>
     <div class="h2">列表切换的动画</div>
   </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, ref, reactive } from "vue";
 
 export default defineComponent({
   components: {},
@@ -20,7 +30,35 @@ export default defineComponent({
     onMounted(() => {
       //
     });
-    return {};
+    let showList = ref(true);
+    const switchShow = () => {
+      //   showList.value = !showList.value;
+      key.value++;
+    };
+    let key = ref(0);
+    interface iTest {
+      [propName: string]: any;
+    }
+    let test: iTest = reactive({
+      list: [],
+    });
+    test.list = [
+      "内容",
+      "内容",
+      "内容",
+      "内容",
+      "内容",
+      "内容",
+      "内容",
+      "内容",
+      "内容",
+    ];
+    return {
+      showList,
+      switchShow,
+      test,
+      key,
+    };
   },
 });
 </script>
@@ -45,5 +83,27 @@ export default defineComponent({
     position: sticky;
     top: 0;
   }
+}
+.transition {
+  min-height: 300px;
+}
+.name-enter-from {
+  opacity: 0;
+  transform: scale(0);
+}
+.name-enter-to,
+.name-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+.name-leave-to {
+  opacity: 0;
+  transform: scale(0);
+}
+.name-enter-active {
+  transition: all 0.35s;
+}
+.name-leave-active {
+  transition: all 0.35s;
 }
 </style>
