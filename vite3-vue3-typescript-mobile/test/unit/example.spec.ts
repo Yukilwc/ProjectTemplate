7ae4e7a61a7d489a7fa0edd8431ab7301718973c
Misaka <misaka1033@gmail.com>
2022-08-01 18:10:39 +0800

    u

--- vite3-vue3-typescript-mobile/test/unit/example.spec.ts
+++ vite3-vue3-typescript-mobile/test/unit/example.spec.ts
@@ -1,17 +1,18 @@
-import {isNumber} from '@/utils/tools'
-import { mount } from '@vue/test-utils'
-import HelloJest from '@/components/HelloJest.vue'
+// import {isNumber} from '@/utils/tools'
+// import { mount } from '@vue/test-utils'
+// import HelloJest from '@/components/HelloJest.vue'
 
-test("is number",()=>{
-    expect(isNumber(3)).toBe(true)
-})
+// test("is number",()=>{
+//     expect(isNumber(3)).toBe(true)
+// })
 
 
-describe('HelloWorld', () => {
-  it('should display header text', () => {
-    const msg = 'Hello Vue 3'
-    const wrapper = mount(HelloJest, { props: { msg } })
+// describe('HelloWorld', () => {
+//   it('should display header text', () => {
+//     const msg = 'Hello Vue 3'
+//     const wrapper = mount(HelloJest, { props: { msg } })
 
-    expect(wrapper.find('h1').text()).toEqual(msg)
-  })
-})
+//     expect(wrapper.find('h1').text()).toEqual(msg)
+//   })
+// })
+export {}
